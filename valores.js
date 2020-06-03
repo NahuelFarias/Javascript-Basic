a5458037cdda55c1169d276d5e35600d38f1665f
Nahuel Farias <nahuelfarias812@gmail.com>
2020-06-03 08:39:31 -0300

    Primer commit

---
+++ valores.js
@@ -0,0 +1,18 @@
+40 // Int
+
+"Nahuel Farias" // String
+
+true
+false // Boleanos
+
+null
+undefined // Valores reservados - Valores vacios - Se usan para reservar memoria
+
+// En conjunto son los valores primitivos
+
+// Valores no primitivos:
+
+[1, 2 ,3]
+{ nombre: "Diego" } // Valor tipo objeto
+
+// Para comprobar los tipos de valores usamos la función "typeof"
